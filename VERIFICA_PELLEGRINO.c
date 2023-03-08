037920612d6271187d745c5f3f8cd00c44eccd3f
notpellee <113350517+notpellee@users.noreply.github.com>
2023-03-08 11:19:25 +0100

    Add files via upload

--- VERIFICA_PELLEGRINO.c
+++ VERIFICA_PELLEGRINO.c
@@ -1,148 +1,174 @@
+/*
+ * Author:Nicola
+ * Date: 08/02/2023
+ */
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#define MAX 5
+#include <stdbool.h>
+#include <malloc.h>
+#include <time.h>
 
-typedef struct pila{
+#define MAX 10
+#define MIN 1
+#define MAXELEMENTI 5
 
-    float num;
-    struct pila *next;
+typedef struct node {
+    int NUMERO_ARRIVO;
+    int VALORE;
+    struct node *next;
+} Node;
 
-}Pila;
-
-typedef struct coda{
-    float num;
-    struct processo *next;
-}Coda;
-
-int is_empty_Coda(Coda* head){
-    if(head == NULL) return 1;
-    else return 0;
+bool isEmpty(Node *list) {
+    return list == NULL;
 }
 
-int is_empty_Pila(Pila* head){
-    if(head == NULL) return 1;
-    else return 0;
-}
-
-void enqueue(Coda** head, Coda** tail, Coda* element){
-    if(is_empty_Coda(*head))
-        *head = element;
+void push(Node **head, Node *el) {
+    if (!isEmpty(*head))
+        el->next = *head;
     else
+        el->next = NULL;
+    *head = el;
+}
+
+Node *pop(Node **head) {
+    if (isEmpty(*head))
+        return NULL;
+    Node *ret = *head;
+    *head = ret->next;
+    return ret;
+}
+
+//funzione che stampa
+void stampaCoda(Node *queue) {
+    if (isEmpty(queue))
+        return;
+    printf("\nnumero coda: %d, %d", queue->NUMERO_ARRIVO, queue->VALORE);
+    if (queue->next != NULL)
+        stampaCoda(queue->next);
+}
+
+//stampa pila
+void stampaPila(Node *l) {
+    //printf("STAMPA CON PROC: \n");
+    while (l != NULL)                                  //puntatore alla lista diversa da NULL
+    {
+        printf("\nstampa pila: %d %d", l->NUMERO_ARRIVO, l->VALORE);
+        l = l->next;                                          //assegna ad l il campo successivo, all'ultima iterazione assegna il campo NULL ed esce dal ciclo
+    }
+}
+
+//funzione enqueu inserisce elementi
+void enqueu(Node **head, Node **tail, Node *element) {
+    if (isEmpty(*head)) {
+        *head = element;
+    } else {
         (*tail)->next = element;
-    
+    }
     *tail = element;
     element->next = NULL;
 }
 
-Coda* dequeue(Coda** head, Coda** tail){
-    Coda* ret = *head;
-
-    if(is_empty_Coda(*head))
+//funzione dequeu per l'estrazione del dato
+Node *dequeu(Node **head, Node **tail) {
+    Node *ret = *head;
+    if (isEmpty(*head))
         return NULL;
-    else
-        *head = ret->next;
-    
-    if(*head == NULL)
+    *head = ret->next;
+
+    if (isEmpty(*head)) {
         *tail = NULL;
-    
+    }
     return ret;
 }
 
-void push(struct pila **head, struct pila *element){
-    if(is_empty_Pila(*head)){
-        *head = element;
-        element -> next = NULL;
-    }
-    else{
-        element->next = *head;
-        *head = element;
-    }
+void freeQueue(Node *queue) {
+    if (queue->next != NULL)
+        freeQueue(queue->next);
+    free(queue);
 }
 
-Pila *pop(Pila **head){
-    Pila *ret = *head;
-    if(is_empty_Pila(*head))
-        return NULL;
-    else
-        *head = ret->next;
-
-    return ret;
-    
+void freePila(Node *l) {
+    if (l->next != NULL)
+        freePila(l->next);
+    free(l);
 }
 
-void stampaCoda(Coda *p){
-    while(p != NULL){
-        printf("%f\n", p->num);
-        p = p->next;
+int main() {
+    //head e tail del nuovo processo
+    Node *h = NULL;
+    Node *t = NULL;
+    int x, x1;
+    bool g=true, pilaControllo=false, codaControllo=false;
+
+    srand(time(NULL));
+    int quanti= MAX-MIN+1;
+    Node *coda = NULL;
+
+    Node *pila = NULL;
+    Node *pila1=NULL;
+
+
+    for(int k=0; k<MAXELEMENTI; k++){
+        printf("entrato\n");
+        x= rand() % quanti+MIN;
+        pila1 = (Node *) malloc(sizeof(Node));
+        pila1->NUMERO_ARRIVO=k+1;
+        pila1->VALORE=x;
+        push(&pila, pila1);
+
+        //-----------------------
+        x1= rand() % quanti+MIN;
+        coda = (Node *) malloc(sizeof(Node));
+        coda->NUMERO_ARRIVO = k+1;
+        coda->VALORE = x1;
+        coda->next=NULL;
+        enqueu(&h, &t, coda);
     }
-}
+    //printf("uscito\n");
+    stampaCoda(h);
+    printf("\n");
+    stampaPila(pila);
+    printf("\n\n");
 
-void stampaPila(Pila *p){
-    while(p != NULL){
-        printf("%f\n", p->num);
-        p = p->next;
-    }
-}
+    while (g == true) {
+        //printf("ecco\n");
+        if (isEmpty(h) || isEmpty(pila)) {
+            if (isEmpty(h)) {
+                printf("\nvince la pila\n");
+                pilaControllo = true;
+                g=false;
+            } else if (isEmpty(pila)) {
+                printf("\nvince la coda\n");
+                codaControllo=true;
+                g=false;
+            } else g = false;
+        }else {
+            Node*exitPila=pop(&pila);
+            Node*exitCoda= dequeu(&h, &t);
+            //stampaPila(pila);
+            //stampaCoda(h);
+            if(exitPila->VALORE!=exitCoda->VALORE){
+                if(exitPila->VALORE>exitCoda->VALORE){
+                    printf("valore della pila %d vince contro il valore della coda %d\n", exitPila->VALORE, exitCoda->VALORE);
+                    exitPila->VALORE-=exitCoda->VALORE;
+                    printf("valore rimanente: %d\n\n", exitPila->VALORE);
+                    push(&pila, exitPila);
 
-
-
-int main(){
-
-    Pila* pila;
-    Coda* coda;
-
-    Pila *testa_pila= NULL;  
-    Pila *coda_pila = NULL;
-
-    Coda *coda_testa = NULL;
-    Coda *coda_coda = NULL;
-
-    coda= (Coda*)malloc(sizeof(Coda));
-    pila= (Pila*)malloc(sizeof(Pila));
-
-    Pila* elemento_coda;
-    Pila* elemento_pila;
-
-    for(int k = 0; k < MAX; k++){
-        printf("inserisci valore da inserire nella coda\n");
-        scanf("%f", &coda->num);
-        printf("inserisci valore da inserire nella pila\n");
-        scanf("%f", &pila->num);
-
-    }
-
-    stampaCoda(coda_testa);
-    stampaPila(testa_pila);
-
-    while(is_empty_Coda(coda_testa) || is_empty_Pila(testa_pila)){
-        elemento_coda = dequeue(&coda_testa, &coda_coda);
-        elemento_pila= pop(&testa_pila);
-
-        printf("numero estratto coda = %f\n", elemento_coda);
-        printf("numero estratto pila %f\n", elemento_pila);
-
-        if(elemento_coda > elemento_pila){
-            
-            enqueue(&coda_testa, &coda_coda, (elemento_coda-elemento_pila));
-            printf("vincitore : %f\n", elemento_coda);
-
-        } else if (elemento_pila > elemento_coda){
-
-            push(&testa_pila, (elemento_pila-elemento_coda));
-            printf("vincitore : %f\n", elemento_pila);
+                }else{
+                    printf("valore della coda %d vince contro il valore della pila %d\n", exitCoda->VALORE, exitPila->VALORE);
+                    exitCoda->VALORE-=exitPila->VALORE;
+                    printf("valore rimanente: %d\n\n", exitCoda->VALORE);
+                    enqueu(&h, &t, exitCoda);
+                }
+            }else{
+                printf("pareggio tra il valore della pila %d e il valore della coda %d\n", exitPila->VALORE, exitCoda->VALORE);
+            }
         }
     }
-
-    if(is_empty_Coda(coda_testa)){
-        printf("struttura vincente = pila\n");
-        stampaCoda(coda_testa);
-    }else if(is_empty_Pila(testa_pila)){
-        printf("struttira vincente = coda\n");
-        stampaPila(testa_pila);
-    }
-
-    
-
-
+    //printf("uscito\n");
+    if(pilaControllo) stampaPila(pila);
+    else if(codaControllo) stampaCoda(h);
+    freeQueue(t);
+    freePila(pila);
+    return 0;
 }
+
